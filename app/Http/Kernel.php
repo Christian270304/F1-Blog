cc680964745f883cea8a8200c907fa776f5afa79
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-04-21 22:03:21 +0200

    feat: Add TrustProxies middleware and set secure session cookie option by default

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
+        \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
