afdf1551f056fdfea9d20f3682fd2e139885933f
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-04-21 21:07:56 +0200

    feat: Force HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+    
+        // Configuración del paginador
         Paginator::defaultView('vendor.pagination.default');
     }
 }
